3602e83d5563570f220e79c32326fe5db007bb1a
Igor Zolotin <goldin7777@gmail.com>
2023-09-12 19:49:55 +0300

    reshil z8

---
+++ seminar1/zadacha8/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+// */
+
+Console.Write("Enter Number: ");
+string strNum = Console.ReadLine();
+int num = int.Parse(strNum);
+
+int EvNum = 2;
+
+if(num > 1)
+{
+    while(EvNum <= num)
+    {
+        Console.Write(EvNum + " ");
+        EvNum = EvNum + 2;
+    }
+}
